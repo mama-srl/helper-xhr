f8cf44161f77bb33eedd337bb8950c24f353e72a
max-favilli <max.favilli@gmail.com>
2021-02-09 13:56:52 +0100

    fixed non compiled dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
       output: {
-        filename: '../helper-xhr.js',
+        filename: './helper-xhr.js',
       },
       entry: './src/helper-xhr.js',
       mode: 'production',
