3f15e7f042ee7044da2532f1028b99f4036ff456
max-favilli <max.favilli@gmail.com>
2021-02-09 02:39:11 +0100

    updated all dependencies

--- helper-xhr.js
+++ helper-xhr.js
@@ -1,109 +1,83 @@
-"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+/***/ "./src/helper-xhr.js":
+/*!***************************!*\
+  !*** ./src/helper-xhr.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n\r\n\r\nclass HttpRequest {\r\n  constructor(method, uri, contentType, headers) {\r\n    this._fetchingPromise = null;\r\n    this.xhr = new XMLHttpRequest();\r\n    if (!!method && !!uri) {\r\n      this.xhr.open(method, uri);\r\n    }\r\n    if (!!contentType) {\r\n      this.xhr.setRequestHeader('Content-Type', contentType);\r\n    }\r\n    if (headers && headers.length) {\r\n      headers.forEach(h => {\r\n        this.xhr.setRequestHeader(h.header, h.value);\r\n      });\r\n    }\r\n  }\r\n\r\n  open(method, uri) {\r\n    if (!!method && !!uri) {\r\n      this.xhr.open(method, uri);\r\n    }\r\n  }\r\n\r\n  setRequestHeader(header, value) {\r\n    if (!!header && !!value) {\r\n      this.xhr.setRequestHeader(header, value);\r\n    }\r\n  }\r\n\r\n  getAllResponseHeaders() {\r\n    return this.headers;\r\n  }\r\n\r\n  fetchFromServer(payload) {\r\n    var ret = null;\r\n\r\n    ret = new Promise((resolve, reject) => {\r\n\r\n      this.xhr.onload = function () {\r\n\r\n        if (this.readyState == this.HEADERS_RECEIVED) {\r\n          this.headers = this.xhr.getAllResponseHeaders();\r\n        }\r\n\r\n        if (this.xhr.status === 200) {\r\n          var d = JSON.parse(this.xhr.responseText, function (k, v) {\r\n            if (!!v && (k === 'createdon' || k === 'updatedon' || k === 'askedon' || k === 'publishedon' || k === 'lastUpdated' && Date.parse(v))) {\r\n              return new Date(v);\r\n            }\r\n            if (!!v && (k === 'json') && typeof v == \"string\") {\r\n              return JSON.parse(v);\r\n            }\r\n            return v;\r\n          });\r\n          resolve(\r\n            {\r\n              json: d,\r\n              headers: this.headers\r\n            }\r\n          )\r\n        } else {\r\n          reject(this.xhr.statusText || (\"status \" + this.xhr.status));\r\n        }\r\n        this._fetchingPromise = null;\r\n      }.bind(this);\r\n\r\n      this.xhr.onerror = (e) => {\r\n        reject(e.target.status);\r\n        this._fetchingPromise = null;\r\n      };\r\n\r\n      if (payload instanceof FormData) {\r\n        this.xhr.send(payload);\r\n      } else {\r\n        this.xhr.send(JSON.stringify(payload));\r\n      }\r\n\r\n      //this.xhr.onreadystatechange = function () {\r\n      //  if (this.readyState == this.HEADERS_RECEIVED) {\r\n      //    console.log(\"headers\", request.getAllResponseHeaders());\r\n      //  }\r\n      //}\r\n    });\r\n\r\n    return ret;\r\n  }\r\n\r\n  send(payload) {\r\n    if (!this._fetchingPromise) {\r\n      this._fetchingPromise = this.fetchFromServer(payload);\r\n    } else {\r\n    }\r\n\r\n    return this._fetchingPromise;\r\n  }\r\n\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HttpRequest);\n\n//# sourceURL=webpack://helper-xhr/./src/helper-xhr.js?");
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+/***/ })
 
-var HttpRequest = function () {
-  function HttpRequest(method, uri, contentType) {
-    _classCallCheck(this, HttpRequest);
-
-    this._fetchingPromise = null;
-    this.xhr = new XMLHttpRequest();
-    if (!!method && !!uri) {
-      this.xhr.open(method, uri);
-    }
-    if (!!contentType) {
-      this.xhr.setRequestHeader('Content-Type', contentType);
-    }
-  }
-
-  _createClass(HttpRequest, [{
-    key: 'open',
-    value: function open(method, uri) {
-      if (!!method && !!uri) {
-        this.xhr.open(method, uri);
-      }
-    }
-  }, {
-    key: 'setRequestHeader',
-    value: function setRequestHeader(header, value) {
-      if (!!header && !!value) {
-        this.xhr.setRequestHeader(header, value);
-      }
-    }
-  }, {
-    key: 'getAllResponseHeaders',
-    value: function getAllResponseHeaders() {
-      return this.headers;
-    }
-  }, {
-    key: 'fetchFromServer',
-    value: function fetchFromServer(payload) {
-      var _this = this;
-
-      var ret = null;
-
-      ret = new Promise(function (resolve, reject) {
-
-        _this.xhr.onload = function () {
-
-          if (this.readyState == this.HEADERS_RECEIVED) {
-            this.headers = this.xhr.getAllResponseHeaders();
-          }
-
-          if (this.xhr.status === 200) {
-            var d = JSON.parse(this.xhr.responseText, function (k, v) {
-              if (!!v && (k === 'createdon' || k === 'updatedon' || k === 'askedon' || k === 'publishedon' || k === 'lastUpdated')) {
-                return new Date(v);
-              }
-              if (!!v && k === 'json' && typeof v == "string") {
-                return JSON.parse(v);
-              }
-              return v;
-            });
-            resolve({
-              json: d,
-              headers: this.headers
-            });
-          } else {
-            reject(this.xhr.statusText);
-          }
-          this._fetchingPromise = null;
-        }.bind(_this);
-
-        _this.xhr.onerror = function (e) {
-          reject(e.target.status);
-          this._fetchingPromise = null;
-        };
-
-        _this.xhr.send(JSON.stringify(payload));
-
-        //this.xhr.onreadystatechange = function () {
-        //  if (this.readyState == this.HEADERS_RECEIVED) {
-        //    console.log("headers", request.getAllResponseHeaders());
-        //  }
-        //}
-      });
-
-      return ret;
-    }
-  }, {
-    key: 'send',
-    value: function send(payload) {
-      if (!this._fetchingPromise) {
-        this._fetchingPromise = this.fetchFromServer(payload);
-      } else {}
-
-      return this._fetchingPromise;
-    }
-  }]);
-
-  return HttpRequest;
-}();
-
-exports.default = HttpRequest;
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/helper-xhr.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+      output: {
+        filename: '../helper-xhr.js',
+      },
+      name: 'first',
+      entry: './src/helper-xhr.js',
+      mode: 'development',
+    },
+  ];
